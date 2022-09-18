3f8335508d7560f9fc2e6b73ae24511350202169
ilknurdemirciarac <ilknurdemirciarac@gmail.com>
2022-09-18 13:50:34 +0300

    İkinci değişim

--- javaProject1/myProject1/src/App.java
+++ javaProject1/myProject1/src/App.java
@@ -1,6 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("merhaba");
         MainFrame myFrame=new MainFrame();
         myFrame.init();
     }
