651042529540ef771c1d6817e197ea8b7e74d36e
ilknurdemirciarac <ilknurdemirciarac@gmail.com>
2022-09-18 13:33:37 +0300

    ilk

---
+++ javaClass.java
@@ -0,0 +1,13 @@
+/**
+ * javaClass
+ * class
+ * main
+ */
+public class javaClass {
+    public static void main(String[] args) {
+        System.out.println("merhaba");
+        
+    }
+
+    
+}
---
+++ javaProject1/myProject1/src/MainFrame.java
@@ -0,0 +1,12 @@
+import javax.swing.JFrame;
+import javax.swing.WindowConstants;
+
+public class MainFrame extends JFrame{
+    public void init(){
+        setTitle("Hello");
+        setSize(600,400);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+    
+}
